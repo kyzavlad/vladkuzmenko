44ae1a79d2d4ef2a46d5ea08c063df2cda06a5a4
kyzavlad <kuzya.universe@gmail.com>
2025-06-19 19:59:51 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,26 @@
 // app/page.tsx
-import Header from "@/components/ui/header";
-import PersonalBrandHero from "@/components/PersonalBrandHero";
-import EducationPlatformSection from "@/components/EducationPlatformSection";
-import AutomationTeaserSection from "@/components/AutomationTeaserSection";
-import SaasLaunchSection from "@/components/SaasLaunchSection";
-import MensCommunitySection from "@/components/MensCommunitySection";
-import MerchPreviewSection from "@/components/MerchPreviewSection";
-import FooterSection from "@/components/FooterSection";
+import { Header } from "@/components/ui/header";
+import { PersonalBrandHero } from "@/components/PersonalBrandHero";
+import { EducationPlatformSection } from "@/components/EducationPlatformSection";
+import { AutomationTeaserSection } from "@/components/AutomationTeaserSection";
+import { SaasLaunchSection } from "@/components/SaasLaunchSection";
+import { MensCommunitySection } from "@/components/MensCommunitySection";
+import { MerchPreviewSection } from "@/components/MerchPreviewSection";
+import { FooterSection } from "@/components/FooterSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
+    // Используем main как flex-контейнер для правильного расположения
+    <main className="flex flex-col min-h-screen bg-background">
       <Header />
-      <PersonalBrandHero />
-      <EducationPlatformSection />
-      <AutomationTeaserSection />
-      <SaasLaunchSection />
-      <MensCommunitySection />
-      <MerchPreviewSection />
+      <div className="flex-grow"> {/* Этот div позволяет футеру быть внизу */}
+        <PersonalBrandHero />
+        <EducationPlatformSection />
+        <AutomationTeaserSection />
+        <SaasLaunchSection />
+        <MensCommunitySection />
+        <MerchPreviewSection />
+      </div>
       <FooterSection />
     </main>
   );
