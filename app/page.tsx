e23e04be03dffbd229d65939cf5a96f429d43613
kyzavlad <kuzya.universe@gmail.com>
2025-06-19 10:20:50 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
 import { Header } from "@/components/ui/header";
 import { HeroSection } from "@/components/HeroSection";
 import { AppsSection } from "@/components/AppsSection";
-import { EnterpriseAISection } from "@/components/EnterpriseAISection";
-import { FeaturesSection } from "@/components/FeaturesSection";
-import { ProjectsSection } from "@/components/ProjectsSection";
 import { MapSection } from "@/components/MapSection";
-import { AudioSection } from "@/components/AudioSection";
-import { PricingSection } from "@/components/PricingSection";
 import { TestimonialsSection } from "@/components/TestimonialsSection";
-import { BlogSection } from "@/components/BlogSection";
 import { SaasLaunchSection } from "@/components/SaasLaunchSection";
-import { CTASection } from "@/components/CTASection";
 import { MensCommunitySection } from "@/components/MensCommunitySection";
 import { FooterSection } from "@/components/FooterSection";
 
@@ -20,16 +13,9 @@ export default function Home() {
       <Header />
       <HeroSection />
       <AppsSection />
-      <FeaturesSection />
-      <EnterpriseAISection />
-      <AudioSection />
-      <ProjectsSection />
-      <PricingSection />
       <TestimonialsSection />
       <MapSection />
-      <BlogSection />
       <SaasLaunchSection />
-      <CTASection />
       <MensCommunitySection />
       <FooterSection />
     </main>
