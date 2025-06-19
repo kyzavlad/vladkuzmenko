e0fb39c66c939742d6d138c1019245605287d9b9
kyzavlad <kuzya.universe@gmail.com>
2025-06-19 21:10:03 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { Header } from "@/components/ui/header";
 import { PersonalBrandHero } from "@/components/PersonalBrandHero";
 import { EducationPlatformSection } from "@/components/EducationPlatformSection";
 import { AutomationTeaserSection } from "@/components/AutomationTeaserSection";
 import { SaasLaunchSection } from "@/components/SaasLaunchSection";
 import { MensCommunitySection } from "@/components/MensCommunitySection";
 import { MerchPreviewSection } from "@/components/MerchPreviewSection";
+import { Header } from "@/components/ui/header";
 import { FooterSection } from "@/components/FooterSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
+    <> 
       <Header />
       <PersonalBrandHero />
       <EducationPlatformSection />
@@ -18,6 +18,6 @@ export default function Home() {
       <MensCommunitySection />
       <MerchPreviewSection />
       <FooterSection />
-    </main>
+    </>
   );
 }
