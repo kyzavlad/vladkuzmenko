17804019f8948cc448c55dcc22fdd16df26093f8
kyzavlad <kuzya.universe@gmail.com>
2025-06-26 03:50:52 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,24 @@
-import { Header } from "@/components/ui/header";
 import { PersonalBrandHero } from "@/components/PersonalBrandHero";
 import { AboutMeSection } from "@/components/AboutMeSection";
 import { EducationPlatformSection } from "@/components/EducationPlatformSection";
+import { AutomationTeaserSection } from "@/components/AutomationTeaserSection";
 import { SaasLaunchSection } from "@/components/SaasLaunchSection";
 import { MensCommunitySection } from "@/components/MensCommunitySection";
-import { AutomationTeaserSection } from "@/components/AutomationTeaserSection";
-import { InstagramSection } from "@/components/InstagramSection";
 import { MerchPreviewSection } from "@/components/MerchPreviewSection";
-import { CTASection } from "@/components/CTASection";
+import { Header } from "@/components/ui/header";
 import { FooterSection } from "@/components/FooterSection";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <>
+    <> 
       <Header />
       <PersonalBrandHero />
       <AboutMeSection />
       <EducationPlatformSection />
+      <AutomationTeaserSection />
       <SaasLaunchSection />
       <MensCommunitySection />
-      <AutomationTeaserSection />
-      <InstagramSection />
       <MerchPreviewSection />
-      <CTASection />
       <FooterSection />
     </>
   );
