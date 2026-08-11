ea7d562136ce37a6da3fb1a7773350249b9dc531
kyzavlad <kuzya.universe@gmail.com>
2026-08-11 08:45:05 +0300

    Route Russian work index to premium showcase

--- app/ru/work/page.tsx
+++ app/ru/work/page.tsx
@@ -1,15 +1,16 @@
 import { I18nProvider } from "@/components/i18n-provider";
-import { PortfolioPage } from "@/components/pages/PortfolioPage";
+import { PortfolioShowcasePage } from "@/components/pages/PortfolioShowcasePage";
 import { pageMeta } from "@/lib/page-meta";
-import { PORTFOLIO_UI, workJsonLd } from "@/lib/portfolio";
+import { PORTFOLIO_UI } from "@/lib/portfolio";
+import { showcaseWorkJsonLd } from "@/lib/portfolio-showcase";
 
 export const metadata = pageMeta("ru", "work", PORTFOLIO_UI.ru.metaTitle, PORTFOLIO_UI.ru.metaDesc);
 
 export default function Page() {
   return (
     <I18nProvider lang="ru">
-      <PortfolioPage />
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(workJsonLd("ru")) }} />
+      <PortfolioShowcasePage />
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(showcaseWorkJsonLd("ru")) }} />
     </I18nProvider>
   );
 }
